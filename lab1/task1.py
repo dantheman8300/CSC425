39f7094bae1674fffe770e3f58a1da25754315b2
Daniel-Astriddao <daniel@astriddao.xyz>
2023-01-15 16:50:05 -0800

    task1 code

---
+++ lab1/task1.py
@@ -0,0 +1,42 @@
+from Crypto.Cipher import AES
+from Crypto.Cipher import ARC4
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+
+key_AES = get_random_bytes(16)
+iv_AES = get_random_bytes(16)
+key_ARC4 = get_random_bytes(5)
+
+def encrypt_AES(plaintext: bytes) -> bytes:
+  paddedPlainText: bytes = pad(plaintext, 16)
+  cipher = AES.new(key_AES, AES.MODE_CBC, iv_AES)
+  return cipher.encrypt(paddedPlainText)
+
+def decrypt_AES(ciphertext: bytes) -> bytes:
+  cipher = AES.new(key_AES, AES.MODE_CBC, iv_AES)
+  return unpad(cipher.decrypt(ciphertext), 16)
+
+def encrypt_ARC4(plaintext: bytes) -> bytes:
+  cipher = ARC4.new(key_ARC4)
+  return cipher.encrypt(plaintext)
+
+def decrypt_ARC4(ciphertext: bytes) -> bytes:
+  cipher = ARC4.new(key_ARC4)
+  return cipher.decrypt(ciphertext)
+
+
+def main():
+  plaintext = b'this is the wireless security lab'
+  
+  print("AES=====================")
+  ciphertext = encrypt_AES(plaintext)
+  print(ciphertext)
+  print(decrypt_AES(ciphertext))
+
+  print("ARC4=====================")
+  ciphertext = encrypt_ARC4(plaintext)
+  print(ciphertext)
+  print(decrypt_ARC4(ciphertext))
+
+
+main()
